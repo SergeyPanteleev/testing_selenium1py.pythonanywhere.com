4c0a53cd4219562f8ce38451a8bba61d64375767
SergeyPanteleev <panteleev.sergey.91@mail.ru>
2019-12-21 13:19:15 +0500

    add test for basket's button

---
+++ test_items.py
@@ -0,0 +1,14 @@
+﻿import time
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_button_add_to_basket(browser):
+    browser.get(link)
+    time.sleep(30)
+    try:
+        browser.find_element_by_css_selector(".btn-add-to-basket")
+    except:
+        assert False, 'The button "Add to basket" is not found'
+    
